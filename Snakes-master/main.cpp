282bed3b961ebbc35336574e83976a73dc738cf8
lixin-ee <61187969+lixin-ee@users.noreply.github.com>
2020-04-22 19:02:26 +0800

    1
    1

---
+++ Snakes-master/main.cpp
@@ -0,0 +1,11 @@
+#include "snake.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Snake w;
+    w.show();
+
+    return a.exec();
+}
---
+++ Snakes-master/snake.h
@@ -0,0 +1,91 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+#include <QWidget>
+#include <QLabel>
+#include <QList>
+#include <QTimer>
+#include <QKeyEvent>
+#include <ctime>
+#include <QPushButton>
+#include <QFont>
+#include <QLineEdit>
+#include <cstdlib>
+#define MAX_X 24
+#define MAX_Y  20
+#define Lable_Size  30 //label快大小
+
+// label类型
+enum Label{
+     // 普通类型，即背景
+    Normal_label,
+    // 边界类型
+    Border_label,
+      // 蛇结点类型
+    Snake_label,
+   // 食物类型
+    Food_label
+};
+
+// 结构体蛇结点
+struct snakeNode {
+    QLabel *label;
+    int type;
+    int x;
+    int y;
+};
+
+namespace Ui {
+class Snake;
+}
+
+class Snake : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Snake(QWidget *parent = 0);
+    ~Snake();
+    void init();            // 初始化游戏
+    void drawBorder();      // 画边界
+    void drawViewArea();    // 画显示区域
+    void showViewArea();    // 显示分数和等级
+    void gameOver();        // 游戏结束
+    void initSnake();       // 初始化蛇
+    void moveSnake();       // 移动蛇
+    void getHeadTail();     // 获取蛇头和蛇尾
+    void createFood();  // 创建食物
+
+    QString part;
+    //void setImage();
+
+protected:
+    void keyPressEvent(QKeyEvent *e);   // 键盘事件
+
+private slots:
+    void snakeMoveSlots();      // 移动操作
+    void startGameSlots();      // 游戏开始
+
+
+
+
+
+private:
+    Ui::Snake *ui;
+
+    QTimer timer;
+    QPushButton *startButton;
+
+    snakeNode *snake_label[MAX_X][MAX_Y];// 储存游戏画面
+    QList<snakeNode*> snake;     // 蛇身
+
+    int dX,dY;      // 移动方向
+    int foodCount,moveSpeed;        // 食物数和速度
+    int level,score;          // 分数和等级
+    snakeNode *head;               // 蛇头和蛇尾指针
+    snakeNode *tail;
+    QLabel *mLabel;           //等级
+    QString viewText;        // 显示区域文本
+
+};
+
+#endif // SNAKE_H
