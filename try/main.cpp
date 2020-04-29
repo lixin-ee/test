6351da574caf87a24c47d507483cd935f9c69ac5
ifnotelse65 <60169892+ifnotelse65@users.noreply.github.com>
2020-04-29 19:53:19 +0800

    a try

---
+++ try/main.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow.h"
+#include <QLabel>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+     QLabel label("Hello, world");
+     label.show();
+    return a.exec();
+}
